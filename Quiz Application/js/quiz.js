651c7402efb45b7ecb7328ed63eca152e28d6eeb
suhasbhat17 <suhasbhat17@gmail.com>
2016-05-02 13:14:34 -0500

    Quiz App Commit

---
+++ Quiz Application/js/quiz.js
@@ -0,0 +1,53 @@
+(function(){
+
+	var app = angular.module('myQuiz',[]);
+    
+    app.controller('QuizController',['$scope','$http','$sce',function($scope,$http,$sce){
+        
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.acticeQuestionAnswered = 0;
+        $scope.percentage = 0;
+        
+        $http.get('https://demo5330878.mockable.io/quizData').then(function(quizData){
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length;
+        });
+        
+         $scope.selectAnswer = function(qIndex,aIndex){
+           var questionState = $scope.myQuestions[qIndex].questionState;
+             
+             if( questionState != 'answered'){
+                 $scope.myQuestions[qIndex].selectedAnswer = aIndex;
+                 var correctAnswer = $scope.myQuestions[qIndex].correct;
+                 $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+                 
+                 if(aIndex === correctAnswer){
+                     $scope.myQuestions[qIndex].correctness = 'correct';
+                     $scope.score +=1;
+                 }else {
+                     $scope.myQuestions[qIndex].correctness = 'incorrect';
+                 }
+                 $scope.myQuestions[qIndex].questionState = 'answered';
+                 
+             }
+             $scope.percentage = (($scope.score / $scope.totalQuestions)*100).toFixed(2);
+        };
+        
+       
+        $scope.isSelected = function(qIndex,aIndex){
+            return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+        };
+        
+        $scope.isCorrect = function(qIndex,aIndex){
+            return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+        };
+        
+        $scope.selectContinue = function(){
+            return $scope.activeQuestion +=1;
+        }
+        
+        
+    }])
+
+})();
